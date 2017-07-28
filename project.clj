19f0454854878decdbce2da530483fc091fedaf2
Erwin Kroon <erwin+git@k3n.nl>
2017-07-29 00:07:41 +0200

    Update clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.csv "0.1.2"]
                  [org.clojure/tools.cli "0.3.1"]
                  [org.clojure/tools.reader "0.8.4"]
